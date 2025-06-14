c9d3be352a8f840eaf3bf87ffdb515e0b8a3a645
connorkuljis <conkuljis@gmail.com>
2025-06-14 18:44:56 +0800

    nvim: created new keymaps for fzf

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -7,6 +7,18 @@ return {
 			vim.keymap.set('n', '<leader>f', ':Files <CR>',
 				{ desc = "Open file picker" }
 			)
+			vim.keymap.set('n', '<leader>b', ':Buffers <CR>',
+				{ desc = "Open file buffer picker" }
+			)
+			vim.keymap.set('n', '<leader>l', ':Lines <CR>',
+				{ desc = "Open line picker" }
+			)
+			vim.keymap.set('n', '<leader>m', ':Marks <CR>',
+				{ desc = "Open mark picker" }
+			)
+			vim.keymap.set('n', '<leader>/', ':Rg <CR>',
+				{ desc = "Open ripgrep picker" }
+			)
 		end
 	},
 }
