85ac6767eeebf37e7c4e19c1d8439e736bad79a8
connorkuljis <conkuljis@gmail.com>
2025-06-14 18:51:24 +0800

    bash: Move GOPATH from $HOME/go to $HOME/.go

--- bash/.bashrc.d/environment.sh
+++ bash/.bashrc.d/environment.sh
@@ -1,11 +1,13 @@
 # ~/.bashrc.d/environment.sh
+export HISTSIZE=100000
+
 export EDITOR="nvim"
 
 # Add system wide go path
 export PATH=$PATH:/usr/local/go/bin 
 
 # Add user programs to go path
-export PATH=$PATH:~/go/bin
+export PATH=$PATH:~/.go/bin
 
 # Source rust/cargo environment
 . "$HOME/.cargo/env"
