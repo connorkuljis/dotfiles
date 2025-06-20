f6d54cdf60c2a2e390ff6c382c609f3febe8efc5
connorkuljis <conkuljis@gmail.com>
2025-06-20 16:39:19 +0800

    nvim: add whichkey plugin

---
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,23 @@
+-- plugins/whichkey.lua
+-- This file configures the 'which-key.nvim' plugin for Neovim.
+
+local whichkey = {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
+}
+
+return whichkey
