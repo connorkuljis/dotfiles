a69d80609ef4de2155d3b61803d001bab0b17f71
connorkuljis <conkuljis@gmail.com>
2025-06-20 16:41:13 +0800

    nvim(fzf): use key table for keymap

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,24 +1,37 @@
 return {
 	{
 		"junegunn/fzf.vim",
-		dependencies = { "junegunn/fzf" },
 		enabled = true,
-		config = function()
-			vim.keymap.set('n', '<leader>f', ':Files <CR>',
-				{ desc = "Open file picker" }
-			)
-			vim.keymap.set('n', '<leader>b', ':Buffers <CR>',
-				{ desc = "Open file buffer picker" }
-			)
-			vim.keymap.set('n', '<leader>l', ':Lines <CR>',
-				{ desc = "Open line picker" }
-			)
-			vim.keymap.set('n', '<leader>m', ':Marks <CR>',
-				{ desc = "Open mark picker" }
-			)
-			vim.keymap.set('n', '<leader>/', ':Rg <CR>',
-				{ desc = "Open ripgrep picker" }
-			)
-		end
+		dependencies = {
+			"junegunn/fzf",
+		},
+		keys = {
+			{
+				"<leader>f",
+				-- conver to lua
+				":Files <CR>",
+				desc = "Open file picker"
+			},
+			{
+				"<leader>b",
+				":Buffers <CR>",
+				desc = "Open file buffer picker"
+			},
+			{
+				"<leader>l",
+				":Lines <CR>",
+				desc = "Open line picker"
+			},
+			{
+				"<leader>m",
+				":Marks <CR>",
+				desc = "Open mark picker"
+			},
+			{
+				"<leader>/",
+				":Rg <CR>",
+				desc = "Open ripgrep picker"
+			},
+		}
 	},
 }
