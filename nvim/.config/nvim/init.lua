af47162aa4a30bddcc1b5ba15d7e5d2cd8d6f6c0
connorkuljis <conkuljis@gmail.com>
2025-08-15 17:21:09 +0800

    feat(nvim): follow system-theme (light/dark) mode on gnome

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,3 +14,34 @@ require("config.autocommand")
 
 -- Set the colorscheme
 vim.cmd([[colorscheme onedark]])
+
+-- Automatic light/dark mode.
+local enableAutoLightDarkMode = true
+local bg = "dark"
+
+if enableAutoLightDarkMode then
+	local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
+	if not handle then
+		error("cannot get gnome interface color theme")
+		return
+	end
+
+	local result = handle:read("*a")
+	handle:close()
+
+	-- trim whitespace/newlines
+	result = result:gsub("%s+", "")
+
+	-- trim single quotes
+	result = result:gsub("'", "")
+
+	local themes = {
+		["prefer-dark"] = "dark",
+		["default"]     = "light",
+		["auto"]        = "light", -- some GNOME versions
+	}
+
+	bg = themes[result]
+end
+
+vim.cmd("set background=" .. bg)
