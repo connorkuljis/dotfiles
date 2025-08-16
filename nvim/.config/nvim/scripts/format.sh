5ad9f14b8afec2fd754e6e2e9b1f166d6d46bf4c
connorkuljis <conkuljis@gmail.com>
2025-08-16 12:52:51 +0800

    chore(nvim): add verbose output to stylua format

--- nvim/.config/nvim/scripts/format.sh
+++ nvim/.config/nvim/scripts/format.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
+
+set -ex
+
 # formats all lua files with stylua
 if ! command -v stylua >/dev/null 2>&1; then
   echo "stylua is not installed. Install it and rerun."
   exit 1
 fi
-stylua .
+
+stylua -v .
